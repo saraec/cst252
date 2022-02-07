335766a073a3fdb1042486a6ee5f9014ddc45482
saraec <sarachristensen1701@gmail.com>
2022-02-07 15:59:30 -0600

    start of lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+// Text on a Curved Path with Animation.
+// Setting text on a circular path and adding an animation that makes the text move along the path infinitely.
+// SaraChristensen. ArianaMoniz.
+// 2.7.2022.
+
+// Make font on a circular path spin
+        //Write text
+        // Create a vector of a curved path
+            // Draw circle in Illustrator and save as a SVG file
+        // Set it on a curved path in CSS
+            // Transform tag or offset-path?
+        // Add animation tag to make text move
