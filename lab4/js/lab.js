d29760283e4d82f33cbe702717e1ba65c8c4a77f
saraec <sarachristensen1701@gmail.com>
2022-02-09 13:18:43 -0600

    Start of lab 5

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -4,9 +4,9 @@
 // 2.7.2022.
 
 // Make font on a circular path spin
-        // Write text
-        // Create a vector of a curved path
-            // Draw circle in Illustrator and save as a SVG file
-        // Set it on a curved path in CSS
-            // Transform tag or offset-path?
-        // Add animation tag to make text move
+    // Write text
+    // Create a vector of a curved path
+        // Draw circle in Illustrator and save as a SVG file
+    // Set it on a curved path in CSS
+        // Transform tag or offset-path?
+    // Add animation tag to make text move
---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+// Data Types and Variables.
+// Declaring and defining variables.
+// SaraChristensen. ArianaMoniz.
+// 2.9.2022.
+
+// Define variables.
+    make = "Chevy";
+    model = "Cruze";
+    color = "Silver";
+    year = 2011;
+
+// calculate
+age = 2022 - year;
+// output
+    document.writeln("Make: " + make + "<br>");
+    document.writeln("Model: " + model + "<br>");
+    document.writeln("Color: " + color + "<br>");
+    document.writeln("Year: " + year + "<br>");
+    document.writeln("Age: " + age + "<br>");
