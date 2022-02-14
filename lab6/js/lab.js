992a7ef02e769000dab9824ea7699256c109854d
saraec <sarachristensen1701@gmail.com>
2022-02-14 12:05:34 -0600

    start of lab 6

---
+++ lab6/js/lab.js
@@ -0,0 +1,10 @@
+// Arrays and Objects. .
+// Creating Arrays and printing Objects.
+// SaraChristensen. ArianaMoniz.
+// 2.14.2022.
+
+// Define variables.
+  var make = "Chevy";
+  var model = "Cruze";
+  var color = "Silver";
+  var year = 2011;
