04462e8c5c3ef69e03e17386408fee7c8db8f228
saraec <sarachristensen1701@gmail.com>
2022-02-14 19:29:36 -0600

    Javascript

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -4,7 +4,19 @@
 // 2.14.2022.
 
 // Define variables.
-  var make = "Chevy";
-  var model = "Cruze";
-  var color = "Silver";
-  var year = 2011;
+myTransport = ["Chevy Cruze", "walk", "carpool"];
+
+myMainRide = {
+  var make = "Chevy",
+  var model = "Cruze",
+  var color = "Silver",
+  var year = 2011,
+  age : function() {
+      return 2022 - age;
+  }
+}
+
+//output
+document.writeln("Types of transportation I use: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
