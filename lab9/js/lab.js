4835d1908c633cfd932178b67154b8018932db9b
saraec <sarachristensen1701@gmail.com>
2022-02-28 13:43:27 -0600

    finished lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -14,6 +14,7 @@
             oneEl.style.color = "white";
             oneEl.style.margin = "7px";
             outputEl.appendChild(oneEl);
+            console.log("Button 1 " + oneEl);
 
             var twoEl = document.createElement("button");
             twoEl.innerHTML = "Stay in the Matrix";
@@ -21,3 +22,18 @@
             twoEl.style.color = "white";
             twoEl.style.margin = "7px";
             outputEl.appendChild(twoEl);
+            console.log("Button 2 " + twoEl);
+
+            // Task x https://www.w3schools.com/jsref/met_document_createattribute.asp
+                function myFunction() {
+                // Create a class attribute:
+                const att = document.createAttribute("style");
+
+                // Set a value of the class attribute
+                att.value = "color: green";
+
+                // Add the class attribute to the first h1;
+                document.getElementsByTagName("h1")[0].setAttributeNode(att);
+                }
+
+                console.log(myFunction);
