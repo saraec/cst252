2a33173d37639d1bb74d1881a90168f188811f74
saraec <sarachristensen1701@gmail.com>
2022-03-02 16:27:52 -0600

    lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -3,32 +3,56 @@
  * Created:   3.2.2022
  **/
 
- // Borrow the sort() function or my anagram() function from Lab 7.
+ // Borrow the sort() function or my anagram() function from Lab 7 (Both functions are from Wes's code)
+ // sortUserName - a function that takes user input and sorts the letters of their name
+ function sortUserName(userName) {
+     // // convert to lower case
+       userName = userName.toLowerCase();
+       console.log("userName =", userName);
+     // // split string to array
+       var nameArray = userName.split('');
+       console.log("nameArray =", nameArray);
+     // // sort the array
+       var nameArraySort = nameArray.sort();
+       console.log("nameArraySort =", nameArraySort);
+     // // join array back to a string
+       var nameSorted = nameArraySort.join('');
+       console.log("nameSorted =", nameSorted);
+     // // Note that I could have done the above lines as a single line:
+       userName.toLowerCase().split("").sort().join("")
+       return userName.toLowerCase().split("").sort().join("");
+ }
 
- // Attach an event listener to your button
+ function randomizeName(userName) {
+     // convert userName string to an array
+     var nameArray = userName.toLowerCase().split("");
+     console.log("nameArray =", nameArray);
+     // shuffle array with our shuffle function
+     var shuffledArray = shuffleArray(nameArray);
+     console.log("shuffledArray =", shuffledArray);
+     var shuffledString = shuffledArray.join("");
+     // shift to Title Case (like a name)
+     var newName = toTitleCase(shuffledString);
+     // return array to a string
+     return newName;
+ }
 
+ // find the button element
+      var buttonEl = document.getElementById("my-button");
+        console.log("button element:", buttonEl);
+      var inputEl = document.getElementById("user-name");
+        console.log("input element:", inputEl);
+      var outputEl = document.getElementById("output");
+        outputEl.style.color = "green";
+        console.log("output element:", outputEl);
+
+
+     // Attach an event listener to your button
+          buttonEl.addEventListener("click", function(){
      // Gets the value of your input field
-
+          var userName = inputEl.value;
      // Runs that value through your sort() or anagram() function and saves the results.
-
+          var newName = sortUserName(userName);
      // Replaces the html in <div id=output> with the results.
-
-     // find the button element
-     buttonEl = document.getElementById("my-button");
-     console.log("button element:", buttonEl);
-     // find the form element
-     inputEl = document.getElementById("user-name");
-     console.log("input element:", inputEl);
-     // find output element
-     outputEl = document.getElementById("output");
-     console.log("output element:", outputEl);
-
-     // add an event listener to button
-     buttonEl.addEventListener("click", function(){
-       // get value from name element
-       var userName = inputEl.value;
-       // modify value - either sort or shuffle
-       var newName = toTitleCase(reorderUserName(userName));
-       // put value in output element
-       outputEl.innerHTML = "<p id=name-results>" + newName + "</p>";
-     });
+           outputEl.innerHTML = "<p id=name-results>" + newName + "</p>";
+         });
