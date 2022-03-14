ff67df6cfa73983ebc6126c908128c3e3cc9da02
saraec <sarachristensen1701@gmail.com>
2022-03-14 12:34:38 -0500

    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,5 +1,5 @@
 function sortingHat(name) {
-  // get langth of name
+  // get length of name
   var len = name.length;
   // do a mod 4 to get house
   var mod = len % 4;
@@ -17,11 +17,14 @@ function sortingHat(name) {
   return houseStr;
 }
 
-$("#my-button").click(function(){
+$("#button").click(function(){
   // need id input in html
   var name = $("#input").val();
-  var house = sortingHouse(name);
-  comsole.log(house);
+  var house = sortingHat(name);
+  console.log(house);
   // need id output in html
   $("#output").html(house);
+   // appends a new styled paragraph to #output that says "The Sorting Hat has sorted you into " + house
+   var newText = "The Sorting Hat has sorted you into " + house;
+  document.getElementById("output").innerHTML = newText;
 })
