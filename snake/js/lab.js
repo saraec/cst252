bff01ee469122b3d234c08f5bab54d37d2c2d8e4
saraec <sarachristensen1701@gmail.com>
2022-04-17 09:03:35 -0500

    new code

--- snake/js/lab.js
+++ snake/js/lab.js
@@ -1,83 +1,72 @@
-/**
- * Author:    Sara Christensen
- * Created:   4.15.2022
- **/
- function birthDate(){
-   // var year=document.getElementById("year").value;
-   var month=document.getElementById("month").value;
-   var day=document.getElementById("day").value;
-   var starArray=["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
-   // var numbers=[0,1,2,3,4,5,6,7,8,9,10,11];
-   var url;
-   console.log(url)
-   // var element=document.getElementById("horoscope");
-   var star=[];
-   if (year===""||month===""||day===""){
-     alert("The information is incomplete!");
-   }
-   else{
-   // for (var i in numbers){
-   // if(numbers[i]===year%12){
-   //   animal=animals[i];
-   //   degree=rotation[i];
-   //   }
-   // }
-   if( (month ==3 && 20<day < 22)||(month==4 && day<20) ){
-   star= starArray[0];
-     url"../img/aries.png";
-   }
-   else if ( (month ==4 && 19 < day )||(month==5 && day<20) ){
-   star= starArray[1];
-     url="../img/taurus.png";
-   }
-   else if ( (month ==5 && 20 < day )||(month==6 && day<21) ){
-   star= starArray[2];
-     url="../img/gemini.png";
-   }
-   else if ( (month ==6 && 20 < day )||(month==7 && day<23) ){
-   star= starArray[3];
-     url="../img/cancer.png";
-   }
-   else if ( (month ==7 && 22 < day )||(month==8 && day<23) ){
-   star= starArray[4];
-     url="../img/leo.png";
-   }
-   else if ( (month ==8 && 22 < day )||(month==9 && day<23) ){
-   star= starArray[5];
-     url="../img/virgo.png";
-   }
-   else if ( (month ==9 && 22 < day )||(month==10 && day<23) ){
-   star= starArray[6];
-     url="../img/libra.png";
-   }
-   else if ( (month ==10 && 22 < day )||(month==11 && day<22) ){
-   star= starArray[7];
-     url="../img/scorpio.png";
-   }
-   else if ( (month ==11 && 21 < day )||(month==12 && day<22) ){
-   star= starArray[8];
-     url="../img/sagittarius.png";
-   }
-   else if ( (month ==12 && 21 < day )||(month==1 && day<20) ){
-   star= starArray[9];
-     url="../img/capricorn.png";
-   }
-   else if ( (month ==1 && 19 < day )||(month==2 && day<19) ){
-   star= starArray[10];
-     url="../img/aquarius.png";
-   }
-   else if ( (month ==2 && 18 < day )||(month==3 && day<21) ){
-   star= starArray[11];
-     url="../img/pisces.png";
-   }
+function birthDate(){
+  var month=$("#month").value;
+  var day=$("#day").value;
+  var starArray=["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
+  var imagesArray=["img/aries.png", "img/Taurus.png", "img/gemini.png", "img/cancer.png", "img/Leo.png", "img/virgo.png", "img/libra.png","img/scorpio.png","img/Sagittarius.png","img/Capricorn.png","img/aquarius.png","img/pisces.png"];
+  var url;
+  var sign=[];
+  var image =[];
+  if (month===""||day===""){
+    alert("Enter the month and date to get your results!");
+  }
+  else{
+  if( (month ==3 && day >= 21)||(month==4 && day<=19) ){
+  sign= starArray[0];
+  image=imagesArray[0];
+  }
+  else if ( (month ==4 && day >= 20 )||(month==5 && day<=20) ){
+  sign= starArray[1];
+  image=imagesArray[1];
+  }
+  else if ( (month ==5 && day >= 21 )||(month==6 && day<=20) ){
+  sign= starArray[2];
+    image=imagesArray[2];
+  }
+  else if ( (month ==6 && day >= 21 )||(month==7 && day<=22) ){
+  sign= starArray[3];
+    image=imagesArray[3];
+  }
+  else if ( (month ==7 && day >= 23 )||(month==8 && day<=22) ){
+  sign= starArray[4];
+    image=imagesArray[4];
+  }
+  else if ( (month ==8 && day >= 23 )||(month==9 && day<=22) ){
+  sign= starArray[5];
+    image=imagesArray[5];
+  }
+  else if ( (month ==9 && day >= 23 )||(month==10 && day<=22) ){
+  sign= starArray[6];
+    image= imagesArray[6];
+  }
+  else if ( (month ==10 && day >= 23 )||(month==11 && day<=21) ){
+  sign= starArray[7];
+    image=imagesArray[7];
+  }
+  else if ( (month ==11 && day >= 22 )||(month==12 && day<=21) ){
+  sign= starArray[8];
+    image=imagesArray[8];
+  }
+  else if ( (month ==12 && day >= 22 )||(month==1 && day<=19) ){
+  sign= starArray[9];
+    image=imagesArray[9];
+  }
+  else if ( (month ==1 && day >= 20 )||(month==2 && day<=18) ){
+  sign= starArray[10];
+    image=imagesArray[10];
+  }
+  else if ( (month ==2 && day >= 19 )||(month==3 && day<=20) ){
+  sign= starArray[11];
+    image=imagesArray[11];
+  }
 
-   document.getElementById("display").innerHTML= star;
-   document.getElementById("zodiac").src=url;
- }
- }
+  $("#display").innerHTML= sign;
+  $("#zodiac").src= image;
+}
+}
+
 
 // button click event
-document.getElementById("button").onclick = function() {
-  birthDate()
-  console.log(birthDate())
+$("#button").onclick = function() {
+ birthDate();
+ console.log(birthDate());
 };
